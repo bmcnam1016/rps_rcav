669814365022e60e18b1ae1419d7317d951b3dbd
bmcnam1016 <bmcnamar@chicagobooth.edu>
2015-10-22 10:06:53 -0500

    Connected my controller file to my erb files.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+def fun
+  render 'fun'
+end
+end
+
+def rock
+render 'rock'
+end
+
+def paper
+render 'paper'
+end
+
+def scissors
+render 'scissors'
+end
