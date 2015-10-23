ff46b2e21f6eaa3f224262309a53701d0b9382ef
bmcnam1016 <bmcnamar@chicagobooth.edu>
2015-10-22 20:05:40 -0500

    Created layout - need to fix instance variables.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,57 @@
 class SessionsController < ApplicationController
 def fun
-  render 'fun'
+
+  @user_move =
+
+  @computer_move = ["rock", "paper", "scissors"].sample
+
+  render 'fun.html.erb'
 end
 end
 
 def rock
-render 'rock'
+
+  @computer_move = ["rock", "paper", "scissors"].sample
+
+  if @computer_move == "rock"
+  result = "You tied!"
+elsif @computer_move == "paper"
+  result = "You lost!"
+elsif @computer_move == "scissors"
+  result = "You won!"
+end
+
+@result = result
+
+render 'rock.html.erb'
 end
 
 def paper
-render 'paper'
+
+@computer_move = ["rock", "paper", "scissors"].sample
+
+  if @computer_move = "rock"
+  puts @win
+elsif @computer_move == "paper"
+  puts @tied
+elsif @computer_move == "scissors"
+  puts @lose
+end
+
+render 'paper.html.erb'
 end
 
 def scissors
+
+  @computer_move = ["rock", "paper", "scissors"].sample
+
+  if @computer_move = "rock"
+  puts @lose
+elsif @computer_move == "paper"
+  puts @win
+elsif @computer_move == "scissors"
+  puts @tied
+end
+
 render 'scissors'
 end
